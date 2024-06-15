d76e92eb4a7f452daf63ba467327a1c3f4feab45
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2024-06-15 12:03:46 +0900

    VersionUP

--- CustomSongTimeEvents/Properties/AssemblyInfo.cs
+++ CustomSongTimeEvents/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CustomSongTimeEvents")]
-[assembly: AssemblyCopyright("Copyright ©  2023 Rynan4818")]
+[assembly: AssemblyCopyright("Copyright ©  2024 Rynan4818")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.1")]
-[assembly: AssemblyFileVersion("0.2.1")]
+[assembly: AssemblyVersion("0.2.2")]
+[assembly: AssemblyFileVersion("0.2.2")]
