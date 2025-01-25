619953f74dd9fb9782114430ec53a1b0ea431f2f
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2025-01-25 20:48:59 +0900

    任意のオブジェクトのActive状態を操作する機能を追加

--- CustomSongTimeEvents/Configuration/SongTimeScriptJson.cs
+++ CustomSongTimeEvents/Configuration/SongTimeScriptJson.cs
@@ -7,10 +7,22 @@ namespace CustomSongTimeEvents.Configuration
     {
         public string SongTime { get; set; }
         public string Event { get; set; }
+        public string Object { get; set; }
+        public bool? ObjectActive { get; set; }
+        public int? ObjectLayer { get; set; }
+    }
+
+    [JsonObject("ObjectList")]
+    public class JSONObjectList
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
     }
 
     public class SongTimeScriptJson
     {
+        [JsonProperty("ObjectList")]
+        public JSONObjectList[] JsonObjectList { get; set; }
         [JsonProperty("TimeScript")]
         public JSONTimeScript[] JsonTimeScript { get; set; }
     }
